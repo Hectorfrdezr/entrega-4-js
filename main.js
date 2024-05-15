9c5b0f1e38ac20fffd6499f4e086de68fd076c7a
hector fernandez <hectorfrdezr@gmail.com>
2024-05-15 18:37:08 -0400

    entrega-4-js

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-const container = document.querySelector(".container");
+document.addEventListener("DOMContentLoaded", function() {
 
+document.querySelector(".btn").addEventListener("click",function(){
 
-fetch(`https://pokeapi.co/api/v2/pokemon/1`)
-.then(Response => Response.json())
-.then(data => container.innerHTML =`
-    
-`)
-.catch(error => console.error(error));
+const pokemon = document.querySelector(".num").value.toLowerCase();
+const url =`https://pokeapi.co/api/v2/pokemon/${pokemon}`;fetch(url)
+    .then(Response =>Response.json())
+    .then(data => {showLetter(data);
+    }).catch(Error => {
+        mostrarError("Tu pokemon no existe")});
+});
+
+function showLetter(data){
+    let pokemonCard = document.querySelector("#pokemon-card");
+    pokemonCard.innerHTML = "";
+
+    let nombre = data.name;
+    let imagen = data.sprites.front_default;
+    let tipos = data.types.map(type => type.type.name).join(", ");
+    let altura = data.height/10;
+    let peso = data.weight/10;
+
+    var cardHTML = `
+    <img src="${imagen}" alt="${nombre}">
+    <h3>${nombre}</h3>
+    <p><strong>Tipo(s):</strong> ${tipos}</p>
+    <p><strong>Altura:</strong> ${altura} m</p>
+    <p><strong>Peso:</strong> ${peso} Kg</p>
+`;
+
+pokemonCard.innerHTML = cardHTML;
+}
+
+function mostrarError(mensaje) {
+    let cardError = document.querySelector("#pokemon-card");
+   cardError.innerHTML = `<h3>Error: ${mensaje}</h3>`}
+});
